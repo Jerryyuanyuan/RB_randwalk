ec650b407414e970aafaf24aa4200e389e14c57a
zhiqiang.yuan <zhiqiang.yuan@cloudcross.com>
2016-07-25 00:26:26 +0800

    提交随机游走算法的若干修改

--- RB_new_main.m
+++ RB_new_main.m
@@ -5,19 +5,19 @@ k = 2;
 N = 80;
 alfa = 0.8;
 r = 3;
-p = 0:0.01:0.2;% 通过参数p或者r变动来观测相变点！
+p = 0.064;% 通过参数p或者r变动来观测相变点！
 tmax = 10^3;    %随机游走法的迭代次数
 M = 100;         %同一组参量产生的实例的个数
 P=zeros(1,length(p));
 index = 1;
-tic
+
 for pi = p
     nums = 1;           %同一组参量生成的RB模型的个数
     sol_nums = 0;       %记录这组条件下的可解实例的个数
     while nums <= M
         [C,Q] = RB_plus(k,N,alfa,r,pi); 
         %生成RB模型的实例
-        [res,sol] = randomwalk_rb_new(C,Q,tmax,N,alfa); %使用随机游走法尝试求解
+        tic;[res,sol] = randomwalk_rb_new(C,Q,tmax,N,alfa); toc%使用随机游走法尝试求解
         if(res == 1)    %找到解那么就将可解数加1
             sol_nums = sol_nums + 1;
         end
@@ -30,5 +30,5 @@ for pi = p
         break;
     end
 end
-toc
+
 plot(p,P,':*');
